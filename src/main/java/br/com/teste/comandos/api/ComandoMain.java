5109aadae42acbb747cbc50a9a5d41eabfc37362
Ramiro Pedrozo <Ramiro.Pedrozo@gmail.com>
2019-02-10 23:30:38 -0200

    implementado classe MAIN

--- src/main/java/br/com/teste/comandos/api/ComandoMain.java
+++ src/main/java/br/com/teste/comandos/api/ComandoMain.java
@@ -1,6 +1,17 @@
 package br.com.teste.comandos.api;
 
+import br.com.teste.comandos.api.commandos.core.builder.ComandosBuilder;
+import br.com.teste.comandos.api.commandos.core.dados.TipoFonte;
+
 public class ComandoMain {
 
-
+    public static void main(String[] args) {
+        ComandosBuilder
+                .criar()
+                .acao(String.join(" ", args))
+                .fonte(TipoFonte.CSV, "/cidade.csv")
+                .construir()
+                .execute()
+                .escreveNoConsole();
+    }
 }
